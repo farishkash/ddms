f0a44f47267a1df3ea94b63a3a4672af2160906c
Farish Kashefinejad <Farish@Farishs-iMac.local>
2016-02-09 20:46:53 -0800

    path

--- app/models/document.rb
+++ app/models/document.rb
@@ -6,6 +6,6 @@ class Document < ActiveRecord::Base
   :path => "#{Rails.root}/public/system/:class/:attachment/:id_partition/:style/:filename"
   #:url => "/system/:attachment/:id/:style/:filename"
 
-  #validates_attachment :doc,
+  validates_attachment :doc
     #:content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
 end
