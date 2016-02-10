9869602702f9a2398dd3c0bae23e9b950d16a9e0
Farish Kashefinejad <Farish@Farishs-iMac.local>
2016-02-09 20:22:46 -0800

    options

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,9 +3,8 @@ class Document < ActiveRecord::Base
   belongs_to :project
 
   has_attached_file :doc,
-  :path => ":rails_root/public/system/:class/:attachment/:id_partition/:style/:filename",
-  :url => "/system/:attachment/:id/:style/:filename"
+  :path => ":rails_root/public/system/:class/:attachment/:id_partition/:style/:filename"
 
-  validates_attachment :doc
-    validates_attachment_content_type :attachment, content_type: ['image/jpeg', 'image/png', 'image/gif', 'application/pdf']
+  #validates_attachment :doc,
+   # :content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
 end
