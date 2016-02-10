d954bf03132a3b4969762c5b600e88fb8fd0d1ea
Farish Kashefinejad <Farish@Farishs-iMac.local>
2016-02-09 19:21:24 -0800

    validation

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,8 +2,8 @@ class Document < ActiveRecord::Base
 	resourcify
   belongs_to :project
 
-  has_attached_file :doc, 
-  :path => ":rails_root/public/system/:class/:attachment/:id_partition/:style/:filename"
+  has_attached_file :doc
+  #:path => ":rails_root/public/system/:class/:attachment/:id_partition/:style/:filename"
 
   #validates_attachment :doc,
    # :content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
