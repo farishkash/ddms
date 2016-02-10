10caac76b4e8fcc3d16741818551bb69d71c7f09
Farish Kashefinejad <Farish@Farishs-iMac.local>
2016-02-09 16:07:53 -0800

    db role

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '45.58.60.40', user: 'deploy', roles: %w{web app}
+server '45.58.60.40', user: 'deploy', roles: %w{web app db}
