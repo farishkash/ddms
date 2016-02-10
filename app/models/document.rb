40f00ac4041fd9747d17b6fe178d8d0c7b3c2c2d
Farish Kashefinejad <Farish@Farishs-iMac.local>
2016-02-09 18:28:29 -0800

    paperclip path

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,7 +2,9 @@ class Document < ActiveRecord::Base
 	resourcify
   belongs_to :project
 
-  has_attached_file :doc
+  has_attached_file :doc, 
+  :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+  :url => "/system/:attachment/:id/:style/:filename"
   validates_attachment :doc,
     :content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
 end
