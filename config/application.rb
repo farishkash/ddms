e5a0ee38a9abab2b4964ef69a8c763cfd7260e21
Farish Kashefinejad <Farish@Farishs-iMac.local>
2016-02-10 07:03:43 -0800

    options

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,14 @@ module Ddms
    Mime::Type.register "application/force-download", :pdf, [], %w(pdf)
 
    Paperclip.options[:command_path] = '/usr/bin'
+
+   require 'paperclip/media_type_spoof_detector'
+module Paperclip
+  class MediaTypeSpoofDetector
+    def spoofed?
+      false
+    end
+  end
+end
   end
 end
