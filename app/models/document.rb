f54f3947a7fa8e6a65da45f204e990e71b167ab3
Farish Kashefinejad <Farish@Farishs-iMac.local>
2016-02-09 20:53:35 -0800

    validation

--- app/models/document.rb
+++ app/models/document.rb
@@ -7,6 +7,6 @@ class Document < ActiveRecord::Base
   #:url => "/system/:attachment/:id/:style/:filename"
 
   validates_attachment :doc
-  do_not_validate_attachment_file_type :doc
+  validates_attachment_content_type :doc, content_type: ['image/jpeg', 'image/png', 'image/gif', 'application/pdf']
     #:content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
 end
