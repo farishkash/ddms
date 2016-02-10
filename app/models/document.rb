439edfe909593fdd4bbef5d702656530375c0f0d
Farish Kashefinejad <Farish@Farishs-iMac.local>
2016-02-09 20:31:03 -0800

    path

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,9 +2,9 @@ class Document < ActiveRecord::Base
 	resourcify
   belongs_to :project
 
-  has_attached_file :doc,
-  :path => "home/deploy/ddms/public/system/:class/:attachment/:id_partition/:style/:filename",
-  :url => "/system/:attachment/:id/:style/:filename"
+  #has_attached_file :doc,
+  #:path => "home/deploy/ddms/public/system/:class/:attachment/:id_partition/:style/:filename",
+  #:url => "/system/:attachment/:id/:style/:filename"
 
   #validates_attachment :doc,
     #:content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
